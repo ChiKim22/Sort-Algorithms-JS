6190f4b71e006aaeab712b0efc5b0123e2ed5997
ChiKim22 <wlsgh2505@gmail.com>
2022-06-14 20:45:14 +0900

    SortAlgorithms

--- HeapSort.js
+++ HeapSort.js
@@ -1,7 +1,35 @@
 function heapSort(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr.length; j++) {
-            
+    let a = [...arr];
+    let len = a.length;
+
+    const heapify = (arr, i) => {
+        let largest = i;
+        let small = 2 * i + 1;
+        let large = 2 * i + 2;
+    
+        if(small < len && arr[largest] > arr[largest]) {
+            largest = small;
+        }
+        if(large < len && arr[largest] > arr[largest]) {
+            largest = large;
+        }
+        if(largest != len) {
+            [arr[largest], arr[i]] = [arr[i], arr[largest]];
+        }
+    };
+
+    for(let i = l/2; i >= 0; i--) {
+        heapify(a, i);
+        for(i = a.length - 1; i > 0; i--) {
+            [a[0], a[i]] = [a[i], a[0]];
+            len--;
+            heapify(a, 0);
         }
     }
-}
+    return arr;
+};
+
+let arr = [1, 6, 7, 8, 9, 3, 5, 4, 2];
+
+console.log(`Original : ${arr}`)
+console.log(`Heap Sorted : ${heapSort(arr)}`);
